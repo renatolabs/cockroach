fe7829e9e8ea6dab5ef60c2bb33fcdfadc6c13b4
Renato Costa <renato@cockroachlabs.com>
2024-07-26 09:58:18 -0400

    roachtest: update PCR mixed-version test
    WIP
    
    Epic: none
    
    Release note: None

--- pkg/cmd/roachtest/tests/mixed_version_c2c.go
+++ pkg/cmd/roachtest/tests/mixed_version_c2c.go
@@ -15,6 +15,7 @@ import (
 	"fmt"
 	"math/rand"
 	"net/url"
+	"sync"
 	"time"
 
 	apd "github.com/cockroachdb/apd/v3"
@@ -22,6 +23,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/registry"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/roachtestutil"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/roachtestutil/mixedversion"
+	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/spec"
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/roachprod"
 	"github.com/cockroachdb/cockroach/pkg/roachprod/install"
@@ -42,13 +44,14 @@ func registerC2CMixedVersions(r registry.Registry) {
 		additionalDuration:        0 * time.Minute,
 		cutover:                   30 * time.Second,
 		skipNodeDistributionCheck: true,
+		clouds:                    registry.OnlyGCE,
 		suites:                    registry.Suites(registry.Nightly),
 	}
 
 	r.Add(registry.TestSpec{
 		Name:             "c2c/mixed-version",
 		Owner:            registry.OwnerDisasterRecovery,
-		Cluster:          r.MakeClusterSpec(sp.dstNodes + sp.srcNodes + 1),
+		Cluster:          r.MakeClusterSpec(sp.dstNodes+sp.srcNodes+1, spec.WorkloadNode()),
 		CompatibleClouds: sp.clouds,
 		Suites:           registry.Suites(registry.Nightly),
 		Run: func(ctx context.Context, t test.Test, c cluster.Cluster) {
@@ -57,22 +60,20 @@ func registerC2CMixedVersions(r registry.Registry) {
 	})
 }
 
-var (
+const (
 	expectedMajorUpgrades = 1
 	minSupportedVersion   = "v23.2.0"
-	sourceTenantName      = "source"
-	destTenantName        = "destination"
+	replicationJobType    = "REPLICATION STREAM INGESTION"
 	fingerprintQuery      = `SELECT fingerprint FROM [SHOW EXPERIMENTAL_FINGERPRINTS FROM VIRTUAL CLUSTER $1 WITH START TIMESTAMP = '%s'] AS OF SYSTEM TIME '%s'`
 )
 
 // TODO (msbutler): schedule upgrades during initial scan and cutover.
 func runC2CMixedVersions(ctx context.Context, t test.Test, c cluster.Cluster, sp replicationSpec) {
-
 	cm := InitC2CMixed(ctx, t, c, sp)
 	cm.SetupHook(ctx)
 	cm.WorkloadHook(ctx)
 	cm.UpdateHook(ctx)
-	cm.Run()
+	cm.Run(ctx, c)
 }
 
 func InitC2CMixed(
@@ -82,6 +83,8 @@ func InitC2CMixed(
 		mixedversion.MinimumSupportedVersion(minSupportedVersion),
 		mixedversion.AlwaysUseLatestPredecessors,
 		mixedversion.NumUpgrades(expectedMajorUpgrades),
+		mixedversion.EnabledDeploymentModes(mixedversion.SharedProcessDeployment),
+		mixedversion.WithTag("source"),
 		mixedversion.DisableSkipVersionUpgrades,
 	)
 
@@ -89,17 +92,26 @@ func InitC2CMixed(
 		mixedversion.MinimumSupportedVersion(minSupportedVersion),
 		mixedversion.AlwaysUseLatestPredecessors,
 		mixedversion.NumUpgrades(expectedMajorUpgrades),
+		mixedversion.EnabledDeploymentModes(mixedversion.SharedProcessDeployment),
+		mixedversion.WithTag("dest"),
 		mixedversion.DisableSkipVersionUpgrades,
 	)
 	return &c2cMixed{
 		sourceMvt:           sourceMvt,
 		destMvt:             destMvt,
+		sourceStartedChan:   make(chan struct{}),
+		destStartedChan:     make(chan struct{}),
 		sp:                  sp,
 		t:                   t,
 		c:                   c,
 		fingerprintArgsChan: make(chan fingerprintArgs, 1),
 		fingerprintChan:     make(chan int64, 1),
 	}
+}
+
+type sourceTenantInfo struct {
+	name  string
+	pgurl *url.URL
 }
 
 type fingerprintArgs struct {
@@ -109,6 +121,8 @@ type fingerprintArgs struct {
 
 type c2cMixed struct {
 	sourceMvt, destMvt  *mixedversion.Test
+	sourceStartedChan   chan struct{}
+	destStartedChan     chan struct{}
 	sp                  replicationSpec
 	t                   test.Test
 	c                   cluster.Cluster
@@ -121,74 +135,126 @@ type c2cMixed struct {
 }
 
 func (cm *c2cMixed) SetupHook(ctx context.Context) {
-	pgUrlChan := make(chan *url.URL)
+	sourceChan := make(chan sourceTenantInfo, 1)
 
-	cm.sourceMvt.OnStartup("source: create app tenant", func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
-		srcNode := cm.c.Node(1)
-		srcClusterSetting := install.MakeClusterSettings()
-		addr, err := cm.c.ExternalPGUrl(ctx, l, srcNode, roachprod.PGURLOptions{})
-		if err != nil {
-			return err
-		}
-		deprecatedStartInMemoryTenant(ctx, cm.t, cm.c, sourceTenantName, cm.c.Range(1, cm.sp.srcNodes))
+	cm.sourceMvt.OnStartup(
+		"generate pgurl",
+		func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
+			// Enable rangefeeds, required for PCR to work.
+			l.Printf("enabling rangefeeds")
+			if err := h.System.Exec(r, "SET CLUSTER SETTING kv.rangefeed.enabled = true"); err != nil {
+				return errors.Wrap(err, "failed to enable rangefeeds")
+			}
+			close(cm.sourceStartedChan)
 
-		pgURL, err := copyPGCertsAndMakeURL(ctx, cm.t, cm.c, srcNode, srcClusterSetting.PGUrlCertsDir, addr[0])
-		if err != nil {
-			return err
+			l.Printf("generating pgurl")
+			srcNode := cm.c.Node(1)
+			srcClusterSetting := install.MakeClusterSettings()
+			addr, err := cm.c.ExternalPGUrl(ctx, l, srcNode, roachprod.PGURLOptions{
+				VirtualClusterName: install.SystemInterfaceName,
+			})
+			if err != nil {
+				return err
+			}
+
+			pgURL, err := copyPGCertsAndMakeURL(ctx, cm.t, cm.c, srcNode, srcClusterSetting.PGUrlCertsDir, addr[0])
+			if err != nil {
+				return err
+			}
+
+			l.Printf("sending pgurl via channel: %s", pgURL)
+			sourceChan <- sourceTenantInfo{name: h.Tenant.Descriptor.Name, pgurl: pgURL}
+
+			l.Printf("waiting for destination tenant to be created")
+			<-cm.destStartedChan
+			l.Printf("done")
+
+			return nil
+		},
+	)
+
+	cm.destMvt.CreateTenantFunc(func(
+		ctx context.Context,
+		l *logger.Logger,
+		r *rand.Rand,
+		h *mixedversion.Helper,
+		name string,
+	) error {
+		l.Printf("waiting to hear from source cluster")
+		sourceInfo := <-sourceChan
+
+		if err := h.System.Exec(r, fmt.Sprintf(
+			"CREATE TENANT %q FROM REPLICATION OF %q ON '%s'",
+			name, sourceInfo.name, sourceInfo.pgurl.String(),
+		)); err != nil {
+			return errors.Wrap(err, "creating destionation tenant")
 		}
-		pgUrlChan <- pgURL
+
+		if err := h.System.QueryRow(r, fmt.Sprintf(
+			"SELECT job_id FROM [SHOW JOBS] WHERE job_type = '%s'",
+			replicationJobType,
+		)).Scan(&cm.ingestionJobID); err != nil {
+			return errors.Wrap(err, "querying ingestion job ID")
+		}
+
+		l.Printf("replication job: %d", cm.ingestionJobID)
+		close(cm.destStartedChan)
 		return nil
-	})
-
-	cm.destMvt.OnStartup("dest: start replication", func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
-		sourcePGURL := <-pgUrlChan
-		return h.System.QueryRow(r, fmt.Sprintf("CREATE TENANT %q FROM REPLICATION OF %q ON '%s'", destTenantName, sourceTenantName, sourcePGURL.String())).Scan(&cm.ingestionJobID)
 	})
 }
 
 func (cm *c2cMixed) WorkloadHook(ctx context.Context) {
 	tpccInitCmd := roachtestutil.NewCommand("./cockroach workload init tpcc").
-		Arg("{pgurl%s:%s}", cm.c.Range(1, cm.sp.srcNodes), sourceTenantName)
+		Arg("{pgurl%s}", cm.c.Range(1, cm.sp.srcNodes))
 	tpccRunCmd := roachtestutil.NewCommand("./cockroach workload run tpcc").
-		Arg("{pgurl%s:%s}", cm.c.Range(1, cm.sp.srcNodes), sourceTenantName).
+		Arg("{pgurl%s}", cm.c.Range(1, cm.sp.srcNodes)).
 		Option("tolerate-errors").
 		Flag("warehouses", 100)
-	cm.workloadStopper = cm.sourceMvt.Workload("tpcc", cm.c.Range(1, cm.sp.srcNodes), tpccInitCmd, tpccRunCmd)
+	cm.workloadStopper = cm.sourceMvt.Workload("tpcc", cm.c.WorkloadNode(), tpccInitCmd, tpccRunCmd)
 }
 
 func (cm *c2cMixed) UpdateHook(ctx context.Context) {
 	destFinalized := make(chan struct{}, 1)
 
 	// For a given major version update this can be called three times: upgrade, downgrade, upgrade again
-	cm.sourceMvt.InMixedVersion("source: wait for dest to finalize", func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
-		if h.Context().Stage == mixedversion.LastUpgradeStage {
-			l.Printf("waiting for destination cluster to finalize upgrade")
-			<-destFinalized
-		}
+	cm.sourceMvt.InMixedVersion(
+		"wait for dest to finalize",
+		func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
+			if h.Context().Stage == mixedversion.LastUpgradeStage {
+				l.Printf("waiting for destination cluster to finalize upgrade")
+				<-destFinalized
+			}
 
-		return nil
-	})
+			return nil
+		},
+	)
 
 	// Called at the end of each major version upgrade.
 	destMajorUpgradeCount := 0
-	cm.destMvt.AfterUpgradeFinalized("dest: cutover and allow source to finalize", func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
-		// Ensure the source always waits to finalize until after the dest finalizes.
-		destFinalized <- struct{}{}
-		destMajorUpgradeCount++
-		if destMajorUpgradeCount == expectedMajorUpgrades {
-			return cm.destCutoverAndFingerprint(ctx, l, r, h)
-		}
-		return nil
-	})
+	cm.destMvt.AfterUpgradeFinalized(
+		"cutover and allow source to finalize",
+		func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
+			// Ensure the source always waits to finalize until after the dest finalizes.
+			destFinalized <- struct{}{}
+			destMajorUpgradeCount++
+			if destMajorUpgradeCount == expectedMajorUpgrades {
+				return cm.destCutoverAndFingerprint(ctx, l, r, h)
+			}
+			return nil
+		},
+	)
 
 	sourceMajorUpgradeCount := 0
-	cm.sourceMvt.AfterUpgradeFinalized("source: fingerprint source", func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
-		sourceMajorUpgradeCount++
-		if sourceMajorUpgradeCount == expectedMajorUpgrades {
-			return cm.sourceFingerprint(ctx, l, r, h)
-		}
-		return nil
-	})
+	cm.sourceMvt.AfterUpgradeFinalized(
+		"fingerprint source",
+		func(ctx context.Context, l *logger.Logger, r *rand.Rand, h *mixedversion.Helper) error {
+			sourceMajorUpgradeCount++
+			if sourceMajorUpgradeCount == expectedMajorUpgrades {
+				return cm.sourceFingerprint(ctx, l, r, h)
+			}
+			return nil
+		},
+	)
 }
 
 func (cm *c2cMixed) destCutoverAndFingerprint(
@@ -197,12 +263,12 @@ func (cm *c2cMixed) destCutoverAndFingerprint(
 
 	var retainedTime hlc.Timestamp
 	if err := h.System.QueryRow(r,
-		`SELECT retained_time FROM [SHOW TENANT $1 WITH REPLICATION STATUS]`, destTenantName).Scan(&retainedTime); err != nil {
+		`SELECT retained_time FROM [SHOW TENANT $1 WITH REPLICATION STATUS]`, h.Tenant.Descriptor.Name).Scan(&retainedTime); err != nil {
 		return err
 	}
 
 	var cutoverStr string
-	if err := h.System.QueryRow(r, "ALTER TENANT $1 COMPLETE REPLICATION TO LATEST", destTenantName).Scan(&cutoverStr); err != nil {
+	if err := h.System.QueryRow(r, "ALTER TENANT $1 COMPLETE REPLICATION TO LATEST", h.Tenant.Descriptor.Name).Scan(&cutoverStr); err != nil {
 		return err
 	}
 	cutover, err := stringToHLC(cutoverStr)
@@ -244,9 +310,33 @@ func (cm *c2cMixed) sourceFingerprint(
 	return nil
 }
 
-func (cm *c2cMixed) Run() {
-	cm.sourceMvt.Run()
-	cm.destMvt.Run()
+func (cm *c2cMixed) Run(ctx context.Context, c cluster.Cluster) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				cm.t.L().Printf("source cluster upgrade failed: %v", r)
+			}
+		}()
+		defer wg.Done()
+		cm.sourceMvt.Run()
+	}()
+
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				cm.t.L().Printf("destination cluster upgrade failed: %v", r)
+			}
+		}()
+		defer wg.Done()
+
+		<-cm.sourceStartedChan
+		cm.destMvt.Run()
+	}()
+
+	wg.Wait()
 }
 
 func stringToHLC(s string) (hlc.Timestamp, error) {
